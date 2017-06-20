93b9f2d00a1297dfbac17fc0d061df6ac895a7d9
Pavel Sereda <pavel.sereda@datalex.com>
2017-06-20 13:34:50 +0300

    Hello cow

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("Hello grass");
+        System.out.println("Hello cow");
     }
 }
